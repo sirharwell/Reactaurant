b073b575bb43f8505dd19a90cc394c7a22981636
Ian Harwell <sirharwell@gmail.com>
2018-03-12 16:13:16 -0600

    navbar

---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NoMatch = () => (
+<div>
+	<link rel="stylesheet" href="css/style.css"></link>
+	<link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,200italic,200,300,300italic,400italic,600,600italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'></link>
+
+
+	<div class="main w3l">
+		<h2>OOPS</h2>
+		<h1> ERROR 404</h1>
+		<h3>"Sorry ! the page you are looking for can't be found"</h3>
+		<a href="#" class="back">BACK TO HOME</a>
+		<div class="social-icons w3">
+			<ul>
+				<li><a class="twitter" href="#"></a></li>
+				<li><a class="facebook" href="#"></a></li>
+				<li><a class="pinterest" href="#"></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+)
+
+export default NoMatch;
