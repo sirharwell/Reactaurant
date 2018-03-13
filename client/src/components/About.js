5ed5a7cc982ccd84175eb384735ed6d6eda94dd3
Ian Harwell <sirharwell@gmail.com>
2018-03-13 01:05:43 -0600

    about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const About = () => (
-  <h1></h1>
+  <h3>  Spicy jalapeno bacon ipsum dolor amet leberkas ipsum elit flank corned beef short ribs sausage incididunt picanha. Quis aute voluptate, prosciutto ea duis elit est. Nisi fatback est quis salami eu porchetta shankle do consequat pork belly nulla ut cupidatat. Qui ball tip hamburger commodo adipisicing ipsum capicola cupim sunt andouille cillum excepteur. Ground round ball tip adipisicing, shank turkey kevin frankfurter filet mignon. In eu laborum chicken dolor. Et magna venison occaecat ribeye.
+<br/>
+<br/>
+  Picanha sint in ullamco beef ribs. Pancetta ut meatloaf in exercitation adipisicing. Incididunt fatback ea duis. Kielbasa culpa pig irure ribeye boudin sed pastrami esse landjaeger hamburger qui swine deserunt burgdoggen. Ut ex nostrud enim ad buffalo fatback in sed strip steak boudin in jerky. Et ut sausage, pork loin andouille landjaeger pastrami salami leberkas spare ribs venison ball tip consectetur.
+<br/>
+<br/>
+  Qui duis corned beef andouille. Dolor qui ullamco, turducken ground round eiusmod reprehenderit flank deserunt pork quis sed tail duis burgdoggen. Tempor meatloaf ullamco brisket tenderloin in enim dolor ex labore cupim. Ham irure cillum dolore short ribs, ad incididunt chicken hamburger bresaola deserunt shank. Consectetur irure bacon in ullamco short loin, kielbasa sunt proident pork alcatra ea rump laborum. Laborum ut rump incididunt tempor kevin id shank porchetta salami labore meatball magna ut doner. Ground round hamburger ea sint sed laborum, velit laboris tempor tongue irure voluptate leberkas jowl.
+<br/>
+<br/>
+  In picanha tongue voluptate sed meatloaf. Magna biltong exercitation culpa flank elit alcatra bacon pork ex. Corned beef pancetta in bresaola leberkas pork loin ut, t-bone brisket biltong salami jerky. Ut sed officia, ham hock excepteur occaecat ullamco consectetur pork chop meatball short ribs. Est meatloaf porchetta laborum minim salami prosciutto magna shankle eiusmod ad cow ball tip ribeye. Bresaola ham officia alcatra qui, exercitation bacon.
+<br/>
+<br/>
+  Turducken ipsum laborum in ham hock. Chicken pork chop buffalo incididunt, bacon pork loin minim turducken beef ribs ea doner dolor esse cupim adipisicing. Lorem veniam enim, bresaola andouille ad shankle mollit qui chuck sunt commodo consequat meatloaf exercitation. Magna consectetur minim, tail ut aliqua tri-tip adipisicing. Venison flank drumstick, beef in bresaola spare ribs enim in tenderloin jowl nostrud capicola esse. Corned beef veniam turkey tri-tip short loin proident, picanha meatloaf pork chop ut cow consequat. Rump shank laboris in ribeye dolore.</h3>
 )
+
 
 export default About
